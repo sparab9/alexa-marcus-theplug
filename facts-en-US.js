d0e9483b8f5a95d6de9f347d1f34b12f3d55df07
sparab9 <sparab18@archmereacademy.com>
2018-08-01 10:08:38 -0400

    Update facts-en-US.js

--- facts-en-US.js
+++ facts-en-US.js
@@ -4,6 +4,6 @@ var data = [
 
 var SKILL_NAME = "Marcus: The Plug";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can tell me what you need help with or to get marcus, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can tell me what device you need help with or to get marcus, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
 var STOP_MESSAGE = "Goodbye, thanks for using my services, Dan!";
