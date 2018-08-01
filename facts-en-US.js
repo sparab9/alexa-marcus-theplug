3ef2be187c862b9c123c3b72b0164f3ba2513c94
sparab9 <sparab18@archmereacademy.com>
2018-08-01 10:08:18 -0400

    Update facts-en-US.js

--- facts-en-US.js
+++ facts-en-US.js
@@ -1,21 +1,9 @@
 var data = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+    'What up doe?',
 ];
 
-var SKILL_NAME = "American Space Facts";
+var SKILL_NAME = "Marcus: The Plug";
 var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_MESSAGE = "You can tell me what you need help with or to get marcus, or, you can say exit... What can I help you with?";
 var HELP_REPROMPT = "What can I help you with?";
-var STOP_MESSAGE = "Goodbye!";
+var STOP_MESSAGE = "Goodbye, thanks for using my services, Dan!";
